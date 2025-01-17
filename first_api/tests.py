75f1b8977a980c865759fcea6a57648ca0258d0d
Husniyor <husniyor09@gmail.com>
2025-01-17 19:19:34 +0500

    added tests

--- first_api/tests.py
+++ first_api/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
-
-# Create your tests here.
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+User = get_user_model()
+class TestViewSet(APITestCase):
+    def setUp(self):
+        self.User = User.objects.create(username="ali",password="13521h0.")
+    def test_user_get(self):
+        url = reverse("user-list")
+        self.client.force_authenticate(self.User)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(len(response.data),1)
+    def test_user_post(self):
+        url = reverse("user-list")
+        self.client.force_authenticate(self.User)
+        data = {
+            "username":"newUser",
+            "password":"newPassword"
+        }
+        response = self.client.post(url,data=data)
+        self.assertEqual(response.status_code,201)
